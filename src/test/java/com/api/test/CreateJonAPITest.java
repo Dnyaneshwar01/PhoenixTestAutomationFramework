44520483d414f98ebaa8d5a7b8022abd1cf646c2
Dnyaneshwar01 <dnyaneshwarubale77@gmail.com>
2025-11-25 08:25:30 +0530

    Introduced the Enums for Handling the codes for the Create Job API Test

--- src/test/java/com/api/test/CreateJonAPITest.java
+++ src/test/java/com/api/test/CreateJonAPITest.java
@@ -3,7 +3,7 @@ package com.api.test;
 import static com.api.utils.DateTimeUtil.*;
 import static io.restassured.RestAssured. *;
 
-import com.api.constant.Role;
+import com.api.constant.*;
 import com.api.records.model.*;
 import com.api.utils.SpecUtil;
 import io.restassured.module.jsv.JsonSchemaValidator;
@@ -21,14 +21,14 @@ public class CreateJonAPITest {
         Customer customer = new Customer("Dnyan", "Ubale","7878787878","","xyz@gmail.com","");
         CustomerAddress customerAddress = new CustomerAddress("c 304","Shanti Sadan","Ramdas Road",
                 "near Bhavin school","Thaltej", "4255252","India","Gujrat");
-        CustomerProduct customerProduct = new CustomerProduct(getTimeWithDaysAgo(10),"11770514993682","18774419973682",
-                "130771979163582",getTimeWithDaysAgo(10),1,1);
-        Problems problems = new Problems(1, "Battery Issue");
+        CustomerProduct customerProduct = new CustomerProduct(getTimeWithDaysAgo(10),"11790514993682","18904419973682",
+                "130779079163582",getTimeWithDaysAgo(10), Product.NEXUS_2.getCode(), Model.NEXUS_2_BLUE.getCode());
+        Problems problems = new Problems(problem.SMARTPHONE_IS_RUNNING_SLOW.getCode(), "Battery Issue");
 
         List<Problems> problemsList = new ArrayList<>();
         problemsList.add(problems);
 
-        CreateJobPayload createJobPayload = new CreateJobPayload(0,2,1,1,customer,
+        CreateJobPayload createJobPayload = new CreateJobPayload(ServiceLocation.SERVICE_LOCATION_A.getCode(), Platform.FRONT_DESK.getCode(), Warrantly_Status.IN_WARRANTY.getCode(), OEM.GOOGLE.getCode(),customer,
                 customerAddress,customerProduct,problemsList);
 
         given().spec(SpecUtil.requestSpecWithAuth(Role.FD,createJobPayload))
