f72bbf4e000e1b048d9483f5e6125fd03bd50c1a
Dnyaneshwar01 <dnyaneshwarubale77@gmail.com>
2026-03-03 23:32:21 +0530

    Added the log4j properly

---
+++ src/test/java/com/listeners/APITestListener.java
@@ -0,0 +1,13 @@
+package com.listeners;
+
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class APITestListener implements ITestListener {
+
+    public void onTestStart(ITestResult result) {
+
+    }
+
+
+}
