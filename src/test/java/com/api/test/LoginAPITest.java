94b1439a12c21b28c69b5c98e700ffa070937316
Dnyaneshwar01 <dnyaneshwarubale77@gmail.com>
2026-03-06 00:58:57 +0530

    revert "Added retry analyze into the framework"
    This reverts commit ffd63a2f19b1687dbb661ca89c9189bd9330f915.

--- src/test/java/com/api/test/LoginAPITest.java
+++ src/test/java/com/api/test/LoginAPITest.java
@@ -34,8 +34,7 @@ public class LoginAPITest {
     @Story("Valid user should be able to login into the system")
     @Description("Verify if FD user is able to login via api")
     @Severity(SeverityLevel.BLOCKER)
-    @Test(description = "Verifying if login is working for FD user", groups = {"api", "regression", "smoke"}
-    , retryAnalyzer = com.api.retry.RetryAnalyzer.class)
+    @Test(description = "Verifying if login is working for FD user", groups = {"api", "regression", "smoke"})
     public void loginAPITest() {
 
         given()
