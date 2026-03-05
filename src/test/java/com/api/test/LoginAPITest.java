ffd63a2f19b1687dbb661ca89c9189bd9330f915
Dnyaneshwar01 <dnyaneshwarubale77@gmail.com>
2026-03-06 00:57:34 +0530

    Added retry analyze into the framework
    created RetryAnalyzer class which will re-run failed test dynamically

--- src/test/java/com/api/test/LoginAPITest.java
+++ src/test/java/com/api/test/LoginAPITest.java
@@ -34,7 +34,8 @@ public class LoginAPITest {
     @Story("Valid user should be able to login into the system")
     @Description("Verify if FD user is able to login via api")
     @Severity(SeverityLevel.BLOCKER)
-    @Test(description = "Verifying if login is working for FD user", groups = {"api", "regression", "smoke"})
+    @Test(description = "Verifying if login is working for FD user", groups = {"api", "regression", "smoke"}
+    , retryAnalyzer = com.api.retry.RetryAnalyzer.class)
     public void loginAPITest() {
 
         given()
